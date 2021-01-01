a263e77b5ce274fc690b31e75d2990dbedf01b95
Call Vin <kanggara75@gmail.com>
2021-01-02 03:06:41 +0700

    Finish Intro Page And add some component lib [ constant, enum, routes, size_config, theme, and in components folder at lib ]

---
+++ lib/screen/intro_page/components/intro_content.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:thesis_app/size_config.dart';
+
+class SplashContent extends StatelessWidget {
+  const SplashContent({
+    Key key,
+    this.text,
+    this.image,
+  }) : super(key: key);
+  final String text, image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Spacer(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "The",
+              style: TextStyle(
+                fontSize: getProportionateScreenWidth(36),
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "SiS",
+              style: TextStyle(
+                fontSize: getProportionateScreenWidth(36),
+                color: Color.fromARGB(255, 68, 213, 173),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+        Text(
+          text,
+          textAlign: TextAlign.center,
+        ),
+        Spacer(flex: 2),
+        Image.asset(
+          image,
+          height: getProportionateScreenHeight(265),
+          width: getProportionateScreenWidth(235),
+        ),
+      ],
+    );
+  }
+}
