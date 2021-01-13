cffdffec018db2f78880a4dfe8d08fb2307cf975
Call Vin <kanggara75@gmail.com>
2021-01-13 21:33:29 +0700

    1. Move State Splash Body to SplashCntroller 2. Add auto redirect using share preferences

--- lib/controllers/Splash.dart
+++ lib/controllers/Splash.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:thesis_app/controllers/User.dart';
+import 'package:thesis_app/controllers/Intro.dart';
+import 'package:thesis_app/controllers/Admin.dart';
 import 'package:thesis_app/config/size_config.dart';
 import 'package:thesis_app/views/splash/index.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashScreen extends StatelessWidget {
   static String routeName = "/splash";
@@ -10,5 +14,33 @@ class SplashScreen extends StatelessWidget {
     return Scaffold(
       body: SplashBody(),
     );
+  }
+}
+
+abstract class SplashController extends State<SplashBody> {
+  var intro, status;
+  @override
+  void initState() {
+    Future.delayed(Duration(milliseconds: 1500), () {
+      getPref();
+    });
+    super.initState();
+  }
+
+  getPref() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    setState(() {
+      intro = preferences.getInt("intro");
+      status = preferences.getInt("status");
+      if (intro == 1) {
+        if (status == 2) {
+          Navigator.pushReplacementNamed(context, UserScreen.routeName);
+        } else if (status == 1) {
+          Navigator.pushReplacementNamed(context, AdminScreen.routeName);
+        }
+      } else {
+        Navigator.pushReplacementNamed(context, IntroScreen.routeName);
+      }
+    });
   }
 }
--- lib/views/splash/index.dart
+++ lib/views/splash/index.dart
@@ -1,21 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
-import 'package:thesis_app/controllers/Intro.dart';
+import 'package:thesis_app/controllers/Splash.dart';
 
 class SplashBody extends StatefulWidget {
   @override
   _SplashBodyState createState() => _SplashBodyState();
 }
 
-class _SplashBodyState extends State<SplashBody> {
-  @override
-  void initState() {
-    Future.delayed(Duration(milliseconds: 1500), () {
-      Navigator.pushReplacementNamed(context, IntroScreen.routeName);
-    });
-    super.initState();
-  }
-
+class _SplashBodyState extends SplashController {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
