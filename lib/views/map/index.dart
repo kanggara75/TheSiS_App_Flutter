a75b49891d321da74a4242d2e24b705229615f88
Call Vin <kanggara75@gmail.com>
2021-01-16 08:46:56 +0700

    Mennambahkan fitur marker pada map

--- lib/views/map/index.dart
+++ lib/views/map/index.dart
@@ -1,4 +1,7 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:here_sdk/mapview.dart';
 import 'package:here_sdk/core.dart';
 
@@ -8,6 +11,17 @@ class MapPage extends StatelessWidget {
     return HereMap(
       onMapCreated: onMapCreated,
     );
+  }
+
+  Future<void> drawMarker(HereMapController hereMapController, int drawOrder,
+      GeoCoordinates geoCoordinates) async {
+    ByteData fileData = await rootBundle.load("assets/icons/marker.png");
+    Uint8List pixelData = fileData.buffer.asUint8List();
+    MapImage mapImage =
+        MapImage.withPixelDataAndImageFormat(pixelData, ImageFormat.png);
+    MapMarker mapMarker = MapMarker(geoCoordinates, mapImage);
+    mapMarker.drawOrder = drawOrder;
+    hereMapController.mapScene.addMapMarker(mapMarker);
   }
 
   void onMapCreated(HereMapController hereMapController) {
@@ -20,6 +34,8 @@ class MapPage extends StatelessWidget {
         }
       },
     );
+
+    drawMarker(hereMapController, 0, GeoCoordinates(0.4706872, 101.3561268));
 
     double distanceInMeter = 800;
 
