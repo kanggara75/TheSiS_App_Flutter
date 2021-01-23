0d8a86d90fd8d6201b7430a91697af134abfb6a5
Call Vin <kanggara75@gmail.com>
2021-01-23 19:30:19 +0700

    Remove Login Success Screen

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -1,5 +1,4 @@
 // ignore: avoid_web_libraries_in_flutter
-
 import 'package:flutter/widgets.dart';
 import 'package:thesis_app/controllers/Splash.dart';
 import 'package:thesis_app/controllers/Intro.dart'; //Intro
@@ -13,7 +12,6 @@ final Map<String, WidgetBuilder> routes = {
   SplashScreen.routeName: (context) => SplashScreen(),
   IntroScreen.routeName: (context) => IntroScreen(), //Intro_Screen
   LoginScreen.routeName: (context) => LoginScreen(), //Login_Screen
-  LoginSuccessScreen.routeName: (context) => LoginSuccessScreen(),
   UserScreen.routeName: (context) => UserScreen(),
   AdminScreen.routeName: (context) => AdminScreen(),
   ForgotScreen.routeName: (context) => ForgotScreen(),
