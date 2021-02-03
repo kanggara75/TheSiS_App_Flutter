f9d6d5cf1fe3ea3dc3729ffd2cf06a27ad596672
Call Vin <kanggara75@gmail.com>
2021-02-03 19:02:31 +0700

    Move TheSiS To Title method

--- lib/views/dashboard/index.dart
+++ lib/views/dashboard/index.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:thesis_app/views/map/counter.dart';
 import 'package:thesis_app/views/map/distance.dart';
-import 'package:thesis_app/config/size_config.dart';
 import 'package:thesis_app/views/control/index.dart';
+import 'package:thesis_app/views/dashboard/title.dart';
 import 'package:thesis_app/controllers/Dashboard.dart';
 import 'package:thesis_app/views/dashboard/fl_chart.dart';
 import 'package:thesis_app/views/control/deviceStatus.dart';
@@ -20,30 +20,7 @@ class _DashboardBodyState extends DashboardController {
         //Tittle Section
         Flexible(
           flex: 2,
-          child: Container(
-            color: Colors.transparent,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "   The",
-                  style: TextStyle(
-                    fontSize: getProportionateScreenWidth(36),
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  "SiS   ",
-                  style: TextStyle(
-                    fontSize: getProportionateScreenWidth(36),
-                    color: Color.fromARGB(255, 68, 213, 173),
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-          ),
+          child: TitleBar(),
         ),
         //Status Section
         Flexible(
