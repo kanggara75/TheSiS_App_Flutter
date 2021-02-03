495e6887f0e5cf2ff89701e86a214587b83265b6
Call Vin <kanggara75@gmail.com>
2021-02-03 22:05:53 +0700

    Add last 5 Coordinat

---
+++ lib/views/map/lastCoor.dart
@@ -0,0 +1,232 @@
+import 'package:flutter/material.dart';
+import 'package:thesis_app/config/size_config.dart';
+import 'package:thesis_app/controllers/Map.dart';
+
+class LastCoordinat extends StatefulWidget {
+  @override
+  _LastCoordinatState createState() => _LastCoordinatState();
+}
+
+class _LastCoordinatState extends LastCoordController {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: getProportionateScreenHeight(270),
+      width: getProportionateScreenWidth(double.infinity),
+      margin: EdgeInsets.fromLTRB(15, 0, 15, 0),
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Color.fromARGB(255, 189, 178, 255),
+        ),
+        borderRadius: BorderRadius.circular(20),
+        color: Color.fromARGB(255, 189, 178, 255),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(
+            "5 Koordinat Terakhir",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 25,
+            ),
+          ),
+          isShowingMainData
+              ? Table(
+                  columnWidths: {
+                    0: FlexColumnWidth(0.5),
+                    1: FlexColumnWidth(1),
+                    2: FlexColumnWidth(1),
+                    3: FlexColumnWidth(1),
+                  },
+                  border: TableBorder(
+                      horizontalInside: BorderSide(
+                        color: Color.fromARGB(255, 7, 30, 34),
+                      ),
+                      verticalInside: BorderSide(
+                        color: Color.fromARGB(255, 255, 255, 252),
+                      )),
+                  children: [
+                    headerTable(),
+                    tableContent(
+                      1,
+                      lat0,
+                      lon0,
+                      time0,
+                    ),
+                    tableContent(
+                      2,
+                      lat1,
+                      lon1,
+                      time1,
+                    ),
+                    tableContent(
+                      3,
+                      lat2,
+                      lon2,
+                      time2,
+                    ),
+                    tableContent(
+                      4,
+                      lat3,
+                      lon3,
+                      time3,
+                    ),
+                    tableContent(
+                      5,
+                      lat4,
+                      lon4,
+                      time4,
+                    ),
+                  ],
+                )
+              : CircularProgressIndicator(),
+        ],
+      ),
+    );
+  }
+
+  TableRow headerTable() {
+    return TableRow(
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: getProportionateScreenHeight(5),
+            ),
+            Text(
+              "  No.",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ],
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: getProportionateScreenHeight(5),
+            ),
+            Text(
+              "Latitude ",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ],
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: getProportionateScreenHeight(5),
+            ),
+            Text(
+              "Longitude",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ],
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: getProportionateScreenHeight(5),
+            ),
+            Text(
+              "Waktu ",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  TableRow tableContent(int no, double lon, double lat, String time) {
+    return TableRow(
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: getProportionateScreenHeight(5),
+            ),
+            Text(
+              "  $no",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                // color: ,
+                fontSize: 18,
+              ),
+            ),
+          ],
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: getProportionateScreenHeight(5),
+            ),
+            Text(
+              "$lon",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+          ],
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: getProportionateScreenHeight(5),
+            ),
+            Text(
+              "$lat",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+          ],
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: getProportionateScreenHeight(5),
+            ),
+            Text(
+              "$time",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
--- lib/views/user/index.dart
+++ lib/views/user/index.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:thesis_app/controllers/User.dart';
+import 'package:thesis_app/views/dashboard/title.dart';
+import 'package:thesis_app/views/map/lastCoor.dart';
+import 'package:thesis_app/views/profile/userInfo.dart';
 
 class UserBody extends StatefulWidget {
   @override
   _UserBodyState createState() => _UserBodyState();
 }
 
-class _UserBodyState extends State<UserBody> {
+class _UserBodyState extends UserBodyController {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Flexible(
+          flex: 1,
+          child: TitleBar(),
+        ),
+        Flexible(
+          flex: 1,
+          child: UserInfo(),
+        ),
+        //Places Sectin
+        Flexible(
+          flex: 4,
+          child: LastCoordinat(),
+        ),
+        //Acc Chart Section
+        Flexible(
+          flex: 3,
+          child: Container(
+            margin: EdgeInsets.fromLTRB(15, 0, 15, 0),
+            color: Colors.blueAccent,
+            // child: Text("You Are Now in ---"),
+          ),
+        ),
+      ],
+    );
   }
 }
