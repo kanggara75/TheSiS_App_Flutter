e074f8644b9194857420e60feb315debb1e8a7c2
Call Vin <kanggara75@gmail.com>
2021-02-06 21:18:21 +0700

    button for admin. belum selesai
    TODO [HW manager + Admin Panel]

---
+++ lib/views/admin/button.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:thesis_app/config/size_config.dart';
+import 'package:thesis_app/helper/default_button.dart';
+import 'package:thesis_app/models/Locator.dart';
+
+class AdminButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(15, 5, 15, 0),
+      padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25),
+        color: Colors.white24,
+      ),
+      child: ListView(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                // crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: getProportionateScreenWidth(5),
+                  ),
+                  FnButton(
+                    text: "Map List",
+                    color: Color.fromARGB(255, 70, 158, 157),
+                    press: () {
+                      Navigator.pushNamed(context, MapList.routeName);
+                    },
+                  ),
+                  FnButton(
+                    text: "Last Location",
+                    color: Color.fromARGB(255, 70, 158, 157),
+                    press: () {
+                      Navigator.pushNamed(context, Near10.routeName);
+                    },
+                  ),
+                  SizedBox(
+                    width: getProportionateScreenWidth(5),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                // crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: getProportionateScreenWidth(5),
+                  ),
+                  FnButton(
+                    text: "Admin Panel",
+                    color: Color.fromARGB(255, 255, 151, 218),
+                    // press: showToast,
+                  ),
+                  FnButton(
+                    text: "HW Manager",
+                    color: Color.fromARGB(255, 255, 151, 218),
+                    // press: showToast,
+                  ),
+                  SizedBox(
+                    width: getProportionateScreenWidth(5),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
