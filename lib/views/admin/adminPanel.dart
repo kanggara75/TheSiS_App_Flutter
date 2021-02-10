2afc9cfa76720a5a69e63cd0a803f37114b324ca
Call Vin <kanggara75@gmail.com>
2021-02-11 05:29:41 +0700

    update coming soon for hw manager

--- lib/views/admin/adminPanel.dart
+++ lib/views/admin/adminPanel.dart
@@ -125,8 +125,17 @@ class _UserManagerState extends UserManagerController {
 class Setting extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Setting"),
+    return Center(
+      child: Container(
+        child: Text(
+          "Coming Soon",
+          style: TextStyle(
+            color: Colors.red,
+            fontSize: 50,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
     );
   }
 }
